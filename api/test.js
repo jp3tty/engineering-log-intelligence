f2527b244962019c9d279721b6c72e76e0075fb9
Jeremy Petty <91158393+jp3tty@users.noreply.github.com>
2025-09-23 16:15:55 -0700

    Remove test endpoint

--- api/test.js
+++
@@ -1,7 +0,0 @@
-// Simple test endpoint
-export default function handler(req, res) {
-  res.status(200).json({
-    message: 'Test endpoint working',
-    timestamp: new Date().toISOString()
-  });
-}
